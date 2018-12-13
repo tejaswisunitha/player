c68ce81507382b381cdf6d195daad4cae239f7cc
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-13 12:17:43 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,6 +1,12 @@
-n=raw_input()
-l=n.split()
-st=''
-for i in l:
-	st+=i.title()+' '
-print(st)
+try:
+	s1=raw_input()
+	a=s1.split(' ')
+	g=" "
+	for i in range(0,len(a)):
+			a[i]=a[i].capitalize()
+	c=a[0]
+	for i in range(1,len(a)):
+		c=c+g+a[i]
+	print c
+except:
+	print "Invalid"
