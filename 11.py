48163e6a0f1e6e12f55c140bda2feee2b2664554
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-14 10:26:52 +0530

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,3 +1,3 @@
-dict={'Monday':'no','Tuesday':'no','Wednesday':'no','Thursday':'no','Friday':'no','Saturday':'nes','Sunday':'yes'}
+dict={'Monday':'no','Tuesday':'no','Wednesday':'no','Thursday':'no','Friday':'no','Saturday':'yes','Sunday':'yes'}
 s=raw_input()
 print dict[s]
