c5536d22be301c3341b93523e2f8f8c4eb742062
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-14 10:39:12 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int n,k,a[10],b[10],i,x;
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(int j=1;j<=k;j++)
+{
+x=a[n-1];
+for (i = n-1; i > 0; i--)
+{
+      a[i] = a[i-1];
+   a[0] = x;
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d ",a[i]);
+}
+return 0;
+}
