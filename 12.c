0c28cb16f4c181ed62d289fc785d5d65558ee50a
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-14 10:44:51 +0530

    Update 12.c

--- 12.c
+++ 12.c
@@ -10,10 +10,10 @@ scanf("%d",&a[i]);
 for(int j=1;j<=k;j++)
 {
 x=a[n-1];
-for (i = n-1; i > 0; i--)
+for (i=n-1;i>0;i--)
 {
-      a[i] = a[i-1];
-   a[0] = x;
+      a[i]=a[i-1];
+   a[0]=x;
 }
 }
 for(i=0;i<n;i++)
