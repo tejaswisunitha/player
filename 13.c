00903d8d0e6f818334d610ac51280181170f15e9
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-14 10:52:33 +0530

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int n,rev=0,rem,sum=0;
-    scanf("%d",&n);
-    while(n!=0)
+    int m,rev=0,rem,sum=0;
+    scanf("%d",&m);
+    while(m!=0)
     {
-        rem=n%10;
+        rem=m%10;
         sum=sum+(rem*rem);
-        n=n/10;
+        m=m/10;
     }
     printf("%d",sum);
 	return 0;
