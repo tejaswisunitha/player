208c2f01ec5b81cff27e6c1e2bddbbe4476a117f
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-15 12:47:13 +0530

    Update 21.py

--- 21.py
+++ 21.py
@@ -1,5 +1,8 @@
-x1,y1=map(int,raw_input().split(' '))
-x2,y2=map(int,raw_input().split(' '))
-x3,y3=map(int,raw_input().split(' '))
-if (x1==x2 and x2==x3) or(y1==y2 and y2==y3) or (abs(x1-x2)==abs(x2-x3) and abs(y1-y2)==abs(y2-y3)):print("yes")
-else:print("no")
+x1,y1=map(int,input().split(' '))
+x2,y2=map(int,input().split(' '))
+x3,y3=map(int,input().split(' '))
+a=x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2) 
+if (a==0): 
+  print("yes")
+else: 
+  print("no")
