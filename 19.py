d9a91da9802d1b9fc31c1710f8762aee5d1523c1
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2018-12-15 12:11:31 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,13 @@
+c=int(raw_input())
+x=1
+while(x<=c):
+    z=0
+    if(c%x==0):
+        y=1
+        while(y<=x):
+            if(x%y==0):
+                z=z+1
+            y=y+1
+        if(z==2):
+            print(x),
+    x=x+1
