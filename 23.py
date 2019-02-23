9af72562ce7b3d5c6ee8778ea6d8a60cd3fdef20
tejaswini kommanaboina <42573830+tejaswisunitha@users.noreply.github.com>
2019-02-23 12:37:23 +0530

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,12 @@
+#ma
+n,k=map(int,input().split())
+N=[int(x) for x in input().split()]
+k=[int(x) for x in input().split()]
+l1=[]
+for i in range(0,len(k)):
+	N.append(k[i])
+	a=max(N)
+	l1.append(a)
+for i in range(0,len(l1)-1):
+	print(l1[i],end=" ")
+print(l1[-1])	
